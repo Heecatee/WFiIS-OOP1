5e78d8a6d82e57dc6937a0a7010cf06c6acda014
Heecatee <heecatee67@gmail.com>
2019-03-04 21:15:02 +0100

    Update Wrapper.h

--- lab_1/wrappers/Wrapper.h
+++ lab_1/wrappers/Wrapper.h
@@ -2,13 +2,13 @@
 #include <iostream>
 
 class Wrapper {
-	public:
-		virtual ~Wrapper() = default;
+    public:
+        virtual ~Wrapper() = default;
 
-		virtual Wrapper* clone() const = 0;
-		virtual std::ostream& print() const = 0;
+        virtual Wrapper* clone() const = 0;
+        virtual std::ostream& print() const = 0;
 };
   
 static std::ostream& operator<<(std::ostream& out, const Wrapper& wrapper) {
-	return wrapper.print(); 
+    return wrapper.print(); 
 } 
