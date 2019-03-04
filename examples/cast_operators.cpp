2564da5c8facc4623dba6e0200f1a2cf14d94e23
Heecatee <heecatee67@gmail.com>
2019-03-04 18:16:03 +0100

    Update cast_operators.cpp

--- examples/cast_operators.cpp
+++ examples/cast_operators.cpp
@@ -7,7 +7,7 @@ int main()
     	/**
      	 * used to cast compatible types
      	 * for example:  double -> int, long -> char, int* -> void*
-     	 * */
+     	 */
 	const double PI = 3.14159265358979323846264279502;
 	int integer_pi = static_cast<int>(PI);
 	return 0;
@@ -21,7 +21,7 @@ int main()
     	/**
      	 * returns NULL, when casting pointers
     	 * returns std::bad_cast, when casting refecences
-    	 * */
+    	 */
 	Honda *honda = new Honda();
 	Car *car = dynamic_cast<Car*>(honda);
 	delete honda;
@@ -34,7 +34,7 @@ int main()
    	/**
    	 * used to cast incompatible types
     	 * for example: char* -> long
-    	 * */
+    	 */
 	const char *str = "lalala";
 	long int str_addr = reinterpret_cast<long int>(str);
 	cout << str << " (addr): " << str_addr;
@@ -49,7 +49,7 @@ int main()
 {
    	/** 
    	 *  used to cast const T -> T or T -> const T
-   	 * */
+   	 */
 	const char *str = "lalala";
 	write(const_cast<char*>(str));
 
