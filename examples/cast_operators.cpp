794aa824be6d860bf4512a0161e020976d8aca50
Kurek Wioletta <kurek.wioletta97@gmail.com>
2019-03-04 09:34:32 +0100

    adding cast examples

---
+++ examples/cast_operators.cpp
@@ -0,0 +1,55 @@
+// static_cast
+int main() 
+{
+    /**
+     * used to cast compatible types
+     * for example:  double -> int, long -> char, int* -> void*
+     * */
+	const double PI = 3.14159265358979323846264279502;
+	int integer_pi = static_cast<int>(PI);
+	return 0;
+}
+
+// dynamic_cast
+class Car {};
+class Honda : public Car {};
+int main()
+{
+    /**
+     * returns NULL, when casting pointers
+     * returns std::bad_cast, when casting refecences
+     * */
+	Honda *honda = new Honda();
+	Car *car = dynamic_cast<Car*>(honda);
+	delete honda;
+	return 0;
+}
+
+// reinterpret_cast
+int main()
+{
+    /**
+     * used to cast incompatible types
+     * for example: char* -> long
+     * */
+	const char *str = "lalala";
+	long int str_addr = reinterpret_cast<long int>(str);
+	cout << str << " (addr): " << str_addr;
+
+	return 0;
+}
+
+// const_cast
+void write(char *s){ cout << s; }
+
+int main()
+{
+    /** 
+     *  used to cast const T -> T or T -> const T
+     * */
+	const char *str = "lalala";
+	write(const_cast<char*>(str));
+
+	return 0;
+}
+
--- examples/operator_overloading.cpp
+++ examples/operator_overloading.cpp
@@ -57,16 +57,22 @@ Box operator+(const Box& b) { // Binary arithmetic operators
 class X // Binary arithmetic operators (complex+integer)
 {
     public:
-        X& operator+=(const X& rhs) // compound assignment (does not need to be a member,
-        {                           // but often is, to modify the private members)
-            /* addition of rhs to *this takes place here */
+        X& operator+=(const X& rhs) {
+            /**
+             * compound assignment (does not need to be a member,
+             * but often is, to modify the private members)
+             * 
+             * addition of rhs to *this takes place here 
+             * */
             return *this; // return the result by reference
         }
         
-        // friends defined inside class body are inline and are hidden from non-ADL lookup
-        friend X operator+(X lhs,        // passing lhs by value helps optimize chained a+b+c
-                            const X& rhs) // otherwise, both parameters may be const references
-        {
+        friend X operator+(X lhs, const X& rhs){
+            /**
+             * friends defined inside class body are inline and are hidden from non-ADL lookup
+             * passing lhs by value helps optimize chained a+b+c
+             * otherwise, both parameters may be const references
+             * */
             lhs += rhs; // reuse compound assignment
             return lhs; // return the result by value (uses move constructor)
         }
